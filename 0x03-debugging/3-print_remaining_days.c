1a264a71f09d23fa68fe97ddd3def61f3913ad4c
kibumbu <josephkibumbu16@gmail.com>
2022-06-10 19:38:39 -0700

    Taks3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,8 +16,9 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month >= 2 && day >= 60)
 		{
-			 day++;
+			day++;
 		}
+
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
@@ -29,8 +30,8 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			 printf("Day of the year: %d\n", day);
-			 printf("Remaining days: %d\n", 365 - day);
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
