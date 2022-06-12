05875792c56fffd39dfdd2c0b15488121cbfbfd4
kibumbu <josephkibumbu16@gmail.com>
2022-06-12 13:07:25 -0700

    Taks 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * prints the alphabet in lowercase, followed by a new line
+ * Print all the letters except q and e
+ * You can only use putchar twice in your code
+ * Return : Always (0)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+		putchar(ch);
+		}
+	}
+
+	putchar(10); /*this is a ascii code for new line*/
+
+	return (0);
+}
