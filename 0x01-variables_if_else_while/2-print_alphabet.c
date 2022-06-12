6f23e4fc81256e6b689e55d1e0354099704c5586
kibumbu <josephkibumbu16@gmail.com>
2022-06-12 10:47:25 -0700

    Taks 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int ch;
+	for (ch = 97; ch <= 122; ch++)
+	{
+	putchar("\n");
+	}
+	putchar(10); /*this is an ascii code for a new line*/
+
+	return (0); 
+}
