018e9d168509c3345b4f1d1421b9453a94c81e28
kibumbu <josephkibumbu16@gmail.com>
2022-06-14 11:22:31 -0700

    Taks 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main -> Write a C program that prints exactly
+ * Use the function printf
+ * You are not allowed to use the function puts
+ * Your program should return 0
+ * Your program should compile without warning when using the -Wall gcc option
+ * Return: always 0
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
