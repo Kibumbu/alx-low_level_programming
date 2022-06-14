303ec74a3cf8749c1d2baf9af704f32e2bc33788
kibumbu <josephkibumbu16@gmail.com>
2022-06-13 21:41:19 -0700

    Taks 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main  -> prints the alphabet in lowercase
+ * You can only use the putchar function
+ * All your code should be in the main function
+ * You can only use putchar twice in your code
+ * Return: always 0
+ */
 int main(void)
 {
 	int ch;
 
 	for (ch = 97; ch <= 122; ch++)
 	{
-	putchar("\n");
+	putchar(ch);
 	}
-	putchar(10); /*this is an ascii code for a new line*/
+	putchar(1);
 
 	return (0);
 }
