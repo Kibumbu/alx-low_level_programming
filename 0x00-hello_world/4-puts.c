160f1bbe278d1ff5b16cc05b00cb3622ae79e1ba
kibumbu <josephkibumbu16@gmail.com>
2022-06-14 11:05:49 -0700

    Taks 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main -> Write a C program that prints exactly
+ * Use the function puts
+ * You are not allowed to use printf
+ * Your program should end with the value 0
+ * Return: always 0
+ */
 int main(void)
 {
-puts("/""Programming is like building a multilingual puzzle");
-return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
