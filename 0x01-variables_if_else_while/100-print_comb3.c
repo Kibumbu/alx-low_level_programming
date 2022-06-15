f75fd56714387b353719aa3e37b3ff6b8277e2b2
kibumbu <josephkibumbu16@gmail.com>
2022-06-14 18:33:10 -0700

    Taks 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main -> prints all possible different combinations of two digits.
+ * Numbers must be separated by ,, followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Return: always 0
+ */
+int main(void)
+{
+	int ch;
+	int n;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		for (n = 49; n <= 57; ch++)
+		{
+			if (n > ch)
+			{
+				putchar(ch);
+				putchar(n);
+				if (ch != 56 || n != 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+
+	putchar(10); /*this is an ascii code for new line*/
+
+	return (0);
+}
