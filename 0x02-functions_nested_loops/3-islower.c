d548c22026e494e4faddcd709606a9f771855490
kibumbu <josephkibumbu16@gmail.com>
2022-06-15 16:38:33 -0700

    tasks 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,8 @@
 
 /**
  * _islower -> checks if the character is lowercase
- * Prototype: int _islower(int c);
- * Returns 1 if c is lowercase
- * Returns 0 otherwise
- * Return: Always (0)
+ * @c: a character argument
+ * Return: return 1 and 0 depending on condition
  */
 int _islower(int c)
 {
