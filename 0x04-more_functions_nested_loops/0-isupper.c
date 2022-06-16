7db37c09a80ef2624101e38371c9add4a8d73d46
kibumbu <josephkibumbu16@gmail.com>
2022-06-15 20:11:14 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *_isupper -> this a fonction checks for uppercase character
+ *@c: an interger paramer
+ *Return: somethings= but success
+ */
+int _isupper(int c);
+{
+	return (c >= 'A' && c <= 'Z');
+}
