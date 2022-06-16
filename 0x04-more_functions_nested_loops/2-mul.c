8cdb706f798d52a9d174c2ad6ba6183267cf8221
kibumbu <josephkibumbu16@gmail.com>
2022-06-15 20:41:24 -0700

    taks 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul -> multiplies two numbers
+ * @a: interger param1
+ * @b: interger param2
+ * Return: success and intiger
+ */
+int mul(int a, int b);
+{
+	return (a * b);
+}
