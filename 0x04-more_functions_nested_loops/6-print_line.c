60729103df485e7602809e4ea1d71bae1382219e
kibumbu <josephkibumbu16@gmail.com>
2022-06-15 21:29:08 -0700

    Taks 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,19 @@
 #include "main.h"
 
 /**
- *
+ *print_line -> printing line
+ *@n: interger params
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (X = 0; x < n; x++)
+			_putchar(95);
+		_putchar('\n');
+	}
+}
