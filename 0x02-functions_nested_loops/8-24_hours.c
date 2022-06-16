6e7ee616fa73841b3e53c87da06539ca9ba5510f
kibumbu <josephkibumbu16@gmail.com>
2022-06-15 19:14:42 -0700

    taks 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,19 +16,25 @@ void jack_bauer(void)
 			if (i < 10)
 			{
 				_putchar('0');
-				_putchar(i + 60);
-			}
+				_putchar(i + '0');
 			else if (i >= 10)
 			{
 				_putchar((i / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			if (i < 10)
+			{
+				_putchar(":");
+				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
 			}
-			if (j < 10)
+			else if (j >= 10)
 			{
 				_putchar(":");
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
+			_putchar("\n");
 		}
 	}
 }
