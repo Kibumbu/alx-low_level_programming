dd3ed388192d00ac601c997fd0620858afe331a2
kibumbu <josephkibumbu16@gmail.com>
2022-06-16 08:29:49 -0700

    Taks 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square -> printing line
+ * @size: integerr params
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size 0 <= 0)
+		-putchar('\n');
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
