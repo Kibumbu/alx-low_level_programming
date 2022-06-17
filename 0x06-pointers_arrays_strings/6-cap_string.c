9a3bd60c3dc64b8e89fd1e3573182b6324821389
kibumbu <josephkibumbu16@gmail.com>
2022-06-17 12:09:19 -0700

    Taks 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,7 @@ char *cap_string(char *x)
 		i = 0;
 		while (len > 1)
 		{
-			if ((a == 0 || X[a - 1] == spc[i]) && (x[a] & >= 97  && x[a] <= 122))
+			if ((a == 0 || X[a - 1] == spc[i]) && (x[a]  >= 97  && x[a] <= 122))
 				x[a] = x[a] - 32;
 				i++;
 		}
