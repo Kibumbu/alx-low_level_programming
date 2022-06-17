64ef430402401d17c92edc11d3d3eed533cf3572
kibumbu <josephkibumbu16@gmail.com>
2022-06-16 17:44:18 -0700

    taks 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  *rev_string -> reversing a string
@@ -13,7 +14,7 @@ void rev_string(char *s)
 	length2 = 0;
 
 	while (s[length1] != '\0')
-		length1++
+		length1++;
 
 	length2 = length1 - 1;
 	for (i = 0; i < length1 / 2; i++)
